eda6f3d196ed9d5cfd43a748d77660035b3985da
徐红潮 <xuhongchao@ybm100.com>
2019-08-15 19:37:23 +0800

    修改cookie

--- base/get_headers.py
+++ base/get_headers.py
@@ -1,16 +1,18 @@
 
+cookie = "uid=CgoUFF0+0D5uOkYjA0YqAg==; sid=0bfde42bea2b4ae8bfcaf2206b224fd6"
+
 def get_headers(header_flag):
     if header_flag == 'data_form':
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'cookie':"uid=CgoUFF0+0D5uOkYjA0YqAg==; sid=5bbed7dddc4d46b5869094fa9e441adc",
+            'cookie':cookie,
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
             "origin":"https://erp.stage.ybm100.com",
         }
     elif header_flag == 'json':
         headers = {
             'Content-Type': 'application/json; charset=UTF-8',
-            'cookie': "uid=CgoUFF0+0D5uOkYjA0YqAg==; sid=5bbed7dddc4d46b5869094fa9e441adc",
+            'cookie': cookie,
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
             "origin":"https://erp.stage.ybm100.com",
         }
